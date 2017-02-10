7e8a143a752049308b5e9233e0b2601a0539c312
xuyafeng1993 <373712828@qq.com>
2017-02-10 15:21:02 +0800

    first commit

---
+++ r_program_homework_02.R
@@ -0,0 +1,4 @@
+plot(sleep$extra,sleep$ID,main = "Student's Sleep Data",xlab="extra",ylab="ID")
+hist(uspop,main=" Populations Recorded by the US Census",xlab="uspop")
+boxplot(uspop,main=" Populations Recorded by the US Census")
+barplot(table(factor(women$weight)),main = "Average Weights for American Women",xlab="weights",ylab = "popular")
